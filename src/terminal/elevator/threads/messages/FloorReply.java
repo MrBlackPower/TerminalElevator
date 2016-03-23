1accec476852e8292f4a77d62598330aa731e239
fbrunogs <franciscobrunogs@yahoo.com.br>
2016-03-23 17:30:46 +0000

    Adds the classes FloorRequest and FloorReply.

--- src/terminal/elevator/threads/messages/FloorReply.java
+++ src/terminal/elevator/threads/messages/FloorReply.java
@@ -5,10 +5,46 @@
  */
 package terminal.elevator.threads.messages;
 
+import java.util.ArrayList;
+import terminal.elevator.threads.Elevator;
+import terminal.elevator.threads.Person;
+
 /**
  *
  * @author User
  */
 public class FloorReply {
+    private final int id;
+    private final Elevator elevator;
+    private ArrayList<Person> persons;
+
+    static int count = 1;
     
+    public FloorReply(Elevator elevator, ArrayList<Person> persons) {
+        this.elevator = elevator;
+        this.persons = persons;
+        id = count;
+        count = count + 1;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the elevator
+     */
+    public Elevator getElevator() {
+        return elevator;
+    }
+
+    /**
+     * @return the persons
+     */
+    public ArrayList<Person> getPersons() {
+        return persons;
+    }
 }
--- src/terminal/elevator/threads/messages/FloorRequest.java
+++ src/terminal/elevator/threads/messages/FloorRequest.java
@@ -5,10 +5,44 @@
  */
 package terminal.elevator.threads.messages;
 
+import terminal.elevator.threads.Elevator;
+
 /**
  *
  * @author User
  */
 public class FloorRequest {
+    private final int id;
+    private final Elevator elevator;
+    private final int floor;
     
+    static int count = 1;
+    
+    public FloorRequest (Elevator elevator, int floor) {
+        this.elevator = elevator;
+        this.floor = floor;
+        id = count;
+        count = count + 1;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the elevator
+     */
+    public Elevator getElevator() {
+        return elevator;
+    }
+
+    /**
+     * @return the floor
+     */
+    public int getFloor() {
+        return floor;
+    }
 }
